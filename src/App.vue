621204a18da6e9a7fbdd5b4bb45cdba0403d1887
ossema90 <d.ossema.daoud@gmail.com>
2023-02-27 00:18:24 +0100

    Publish
    to do app testing my composition api skills

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+    <input type="text" v-model="add"> <button @click="addTask()">Submit</button>
+    <ul>
+      <li v-for="(task) in list" :key="task">
+        {{ task }}
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script setup >
+
+import { ref } from 'vue';
+let add = "";
+const list = ref(["test1", "test2", "test3"]);
+
+function addTask() {
+  list.value.push(add);
+}
+</script>
+
+<style></style>
